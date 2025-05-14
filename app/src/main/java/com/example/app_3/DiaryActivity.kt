492f2046e05d0e0a392474f52ade236998210f48
eajnnim <kimmj7876@naver.com>
2025-05-15 04:00:09 +0900

    hdhwan

---
+++ app/src/main/java/com/example/app_3/DiaryActivity.kt
@@ -0,0 +1,30 @@
+package com.example.app_3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DiaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_diary)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val toolbar = findViewById<com.google.android.material.appbar.MaterialToolbar>(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+
+        // 뒤로가기 아이콘 클릭 시
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
+    }
+}
